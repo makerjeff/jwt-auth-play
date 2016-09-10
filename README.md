ed2a45c24d4e6e3fb9b627dc63b8e976ced1f09d
Jeff Wu <makerjeffwu@gmail.com>
2016-09-10 14:48:22 -0700

    latest and greatest, with a commandline encrypt and decrypt tool.

--- README.md
+++ README.md
@@ -14,3 +14,15 @@ Playing with JSON Web Tokens authorization.
 - [Mongoose Connection Best Practice](http://theholmesoffice.com/mongoose-connection-best-practice/)
 - [Node.JS encryption and decryption](http://lollyrock.com/articles/nodejs-encryption/)
 - [npm prompt](https://www.npmjs.com/package/prompt): Prompting made easy.
+
+## NOTES
+- Basic Signup Flow:
+    - POST firstname, lastname, email, password
+    - check to see if email exists
+        - if yes, bounce back {error: 'Email already taken'}
+        - if no, hash password with bcrypt before writing to database using Mongoose method, bounce back {success: true}
+            - create a new account with the email as an ID (use Mongoose schema methods to verify the email is unique)
+ 
+ - Basic Login Flow:
+    - POST email, password
+    - check
--- _rnd/cryptographic.js
+++ _rnd/cryptographic.js
@@ -4,10 +4,19 @@
 var crypto      = require('crypto');
 var prompt      = require('prompt');
 var colors      = require('colors');
+var bcrypt      = require('bcrypt-nodejs');
+var mongoose    = require('mongoose');
 
 var algorithm   = 'aes-256-ctr';
 var password    = 'monkeypoo';
 
+var mode        = process.argv[2];
+
+/**
+ * Encrypt using Node.js crypto module.
+ * @param textString Input text string.
+ * @returns {*} Returns encrypted string.
+ */
 function encrypt(textString) {
     var cipher = crypto.createCipher(algorithm, password);
     var crypted = cipher.update(textString, 'utf8', 'hex');
@@ -15,6 +24,11 @@ function encrypt(textString) {
     return crypted;
 }
 
+/**
+ * Decrypt using Node.js crypto module.
+ * @param textString    Text string to decrypt.
+ * @returns {*}         Returns decrypted text string.
+ */
 function decrypt(textString) {
     var decipher = crypto.createDecipher(algorithm, password);
     var dec = decipher.update(textString, 'hex','utf8');
@@ -22,12 +36,22 @@ function decrypt(textString) {
     return dec;
 }
 
-//
-// var hw = encrypt('helloworld!');
-// console.log(hw);
-// console.log(decrypt(hw));
+/**
+ * Use Bcrypt to hash passwords.
+ * @param textString    Input data to hash.
+ * @returns {*}         Returns Bcrypt-ed hash (to store in db).
+ */
+function bcryptHash(textString){
+    return bcrypt.hash(textString,null,null,function(err,hash){
+        return hash;
+    });
+}
 
+function bcryptHashSync(textString){
+    return bcrypt.hashSync(textString);
+}
 
+//enable Prompt.
 prompt.start();
 
 // prompt.get(['username','email'], function(err,result){
@@ -36,30 +60,47 @@ prompt.start();
 //     console.log('email: '.yellow + result.email);
 // });
 
-prompt.get([
-    {
-        name: 'username',
-        description: 'Please enter your username: ',
-        type: 'string',
-        required: true
-    },
-    {
-        name: 'email',
-        description: 'Please enter your email address: ',
-        type: 'string',
-        required: true,
-        message: 'this is a messages!'
-    },{
-        name: 'password',
-        description: 'Please enter a password: ',
-        type: 'string',
-        required: true,
-        hidden: true
-    }
-], function(err, result){
-    console.log('Command line date received: ');
-    console.log('username: '.cyan + result.username);
-    console.log('email: '.cyan + result.email);
-    console.log('password: '.cyan + result.password);
+if(mode == 'ENCRYPT'){
+    //advanced Prompt options in object array
+    prompt.get([
+        {
+            name: 'username',
+            description: 'Please enter your username: ',
+            type: 'string',
+            required: true
+        },{
+            name: 'email',
+            description: 'Please enter your email address: ',
+            type: 'string',
+            required: true,
+            message: 'this is a messages!'
+        },{
+            name: 'password',
+            description: 'Please enter a password: ',
+            type: 'string',
+            required: true,
+            hidden: true
+        }
+    ], function(err, result){
+        console.log('Command line date received: ');
+        console.log('username: '.cyan + encrypt(result.username));
+        console.log('email: '.cyan + encrypt(result.email));
+        console.log('password: '.cyan + bcryptHashSync(result.password));
+    });
 
-});
+}
+
+else if(mode == 'DECRYPT') {
+    prompt.get([
+        {name: 'hashies', description: 'Enter a hash: ', type: 'string'},
+        {name: 'password', description: 'Enter your password: ', type: 'string', hidden: true}
+    ], function(err, result){
+        if(result.password === password) {
+            console.log('Your result: ' + decrypt(result.hashies));
+        } else {
+            console.log('Password is incorrect.'.red);
+        }
+    });
+}
+
+
