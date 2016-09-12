9f6ca9b61ce5080d78c22b4024e7f112848434ca
Jefferson Wu <makerjeffwu@gmail.com>
2016-09-12 12:54:04 -0700

    adding httpbasic.js as a study without Express.

--- _rnd/httpbasic.js
+++ _rnd/httpbasic.js
@@ -4,18 +4,25 @@
 
 //HTTP Basic Server
 
-var http = require('http');
+var http            = require('http');
+var colors          = require('colors');
+var mongodb         = require('mongodb');
+var port            = 4000;
 
-var server = http.createServer(function(req, res){
+// setup
+var MongoClient     = mongodb.MongoClient;
+var mongoUrl        = 'mongodb://localhost/httpbasic';
+
+var server          = http.createServer(function(req, res){
     // stuff happens here...
 
-    res.writeHead(200, {'Content-Type':'application/json', 'X-Powered-By':'Bacon Grease'});
+    res.writeHead(200, {'Content-Type':'text/html', 'X-Powered-By':'Bacon Grease'});
     res.write('<html>');
     res.write('<body>');
 
     res.write('<h1>This is a response without Express!</h1>');
     res.write('<p>');
-    res.write('I feel naked without ')
+    res.write('I feel naked without Express, but also liberating!');
     res.write('</p>');
 
     res.write('</body>');
@@ -23,4 +30,8 @@ var server = http.createServer(function(req, res){
 
     res.end();
 
+});
+
+server.listen(port, function(){
+    console.log('Server is running on port ' + port.toString().blue + '.');
 });
